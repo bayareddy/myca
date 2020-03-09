e17d059a5beb300f0e5411c29e5ffe6e03239895
PardhaSaradhi Personal <mannempardhu@gmail.com>
2020-03-09 16:53:52 +0530

    Initial Commit of UI

--- MyCaUI/src/app/home/home.component.ts
+++ MyCaUI/src/app/home/home.component.ts
@@ -18,20 +18,17 @@ ngOnInit(){
 
  this.data=this.service.messages$.subscribe(data=>{this.data=data
   console.log(data)
-  console.log(this.array)
+
 });
  
 }
 
-array=['a','b','c','d','e']
+
 
   closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     this.service.subjectMethod$('sideNavPardha')
-    let s='manohar'
-
-console.log('splice',this.array.splice(2,3,'p'))
-console.log(this.array)
+   
 
   }
 
